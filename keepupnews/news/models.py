3522d7244a2004fb4b973f46400645add55af7ac
alejandro <luis.vejarano01@correo.usa.edu.co>
2022-07-11 20:21:03 -0500

    Upload model and news view

--- keepupnews/news/models.py
+++ keepupnews/news/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class News(models.Model):
+    title = models.CharField(max_length=255)
+    image = models.ImageField()
+    topic = models.CharField(max_length=255)
+    date = models.DateTimeField(auto_now=True)
+    description = models.TextField()
+    content = models.TextField()
--- keepupnews/news/views.py
+++ keepupnews/news/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from .models import News
 
 def index(request):
-    return render(request, 'index.html')
+    all_news = News.objects.all()
+
+    return render(request, 'index.html', all_news)
