53009d1f1ac6d96fca48aa44f74b4a3912adc976
alejandro <luis.vejarano01@correo.usa.edu.co>
2022-07-11 21:29:16 -0500

    add view and create

--- keepupnews/news/models.py
+++ keepupnews/news/models.py
@@ -5,6 +5,6 @@ class News(models.Model):
     title = models.CharField(max_length=255)
     image = models.ImageField()
     topic = models.CharField(max_length=255)
-    date = models.DateTimeField(auto_now=True)
+    date = models.DateTimeField()
     description = models.TextField()
     content = models.TextField()
--- keepupnews/news/views.py
+++ keepupnews/news/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render
 from .models import News
+from django.utils import timezone
 
 def index(request):
     all_news = News.objects.all()
+    return render(request, 'index.html', {'all_news':all_news})
 
-    return render(request, 'index.html', all_news)
+def create(request):
+    return render(request, 'create.html')
+
+def add(request):
+    title = request.POST['title']
+    image = request.POST['img']
+    topic = request.POST['topic']
+    description = request.POST['description']
+    content = request.POST['content']
+    new = News(title=title, image=image, topic=topic, date=timezone.now(), description=description, content=content)
+    new.save()
+    return render(request, 'create.html', {"status":200})
+
