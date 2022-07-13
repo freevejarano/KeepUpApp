44bf712e8acf2fca4deaa5518db4f33f1db9b90c
alejandro <luis.vejarano01@correo.usa.edu.co>
2022-07-13 16:55:05 -0500

    Final version

--- keepupnews/news/static/js/functions.js
+++ keepupnews/news/static/js/functions.js
@@ -7,12 +7,10 @@ window.onload = function() {
       offer.innerHTML += "<img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR09guhYQlcWXypvKIMNi5YvWX3HmtSVjsKSQ&usqp=CAU' width='300' height='300'/>";
     }
   }else if(document.title=="Keep Up - Editar"){
-    var cod = document.getElementById("cod");
-    if(cod.value != 200){ // When the window just loads the info but doesn´t edit it, fix a html problem
+    // When the window just loads the info but doesn´t edit it, fix a html problem
       var title = document.getElementsByName("title")[0];
       title.value=document.getElementById("prov").innerText;
       var desc = document.getElementsByName("description")[0];
       desc.value=document.getElementById("prov2").innerText;
-    }
   }
 };
